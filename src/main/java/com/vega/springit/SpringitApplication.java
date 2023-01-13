8d78fee30edd710cb38d4bfd2801b16cde5f17f6
Pedro Pires <aluno21215@ipt.pt>
2023-01-13 10:57:18 +0000

    Adding a README file with some initial documentation.

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringitApplication {
 
-	public String ola;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 
