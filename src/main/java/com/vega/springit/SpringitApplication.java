de738ef8598f8f77fb93a85afafd85d647a15c53
Pedro Pires <aluno21215@ipt.pt>
2023-01-13 10:30:52 +0000

    Commit 2, teste!

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringitApplication {
 
+	public String ola;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+
+
 	}
 
 }
