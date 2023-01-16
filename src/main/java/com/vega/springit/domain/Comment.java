a20b31bf243c74dd13a44e1e86530e66fe2c7cca
Pedro Pires <aluno21215@ipt.pt>
2023-01-16 08:44:33 +0000

    Adding mappings from link->comment

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -18,5 +19,9 @@ public class Comment {
     private Long id;
 
     private String body;
+
+    //link
+    @ManyToOne
+    private Link link;
 
 }
--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -3,12 +3,13 @@ package com.vega.springit.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -26,4 +27,8 @@ public class Link {
     @NonNull
     private String url;
 
-}
+
+    //comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
+};
